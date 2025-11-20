b9e164d43336fe03830777eccbff0f589ca346e8
Anas Sultan <107485610+anassultanali@users.noreply.github.com>
2025-11-20 13:19:24 +0300

    Start...

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <div className="calss flex  justify-center ">
+
+      <h1 className="text-3xl   text-praimary font-bold underline">
+        Stay Safe!{" "}
+      </h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
