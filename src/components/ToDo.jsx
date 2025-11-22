0a9044e9d23dac1ceef10ccd543b3e96ed271881
Anas Sultan <107485610+anassultanali@users.noreply.github.com>
2025-11-22 17:23:50 +0300

    47:53

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,27 @@
+import { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import ToDoList from "./ToDoList";
 
 export default function ToDo() {
+  const [tasks, setTasks] = useState([]);
+
+  const inputRef = useRef();
+  const add = () => {
+    let inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+    let newTask = {
+      id: Date.now(),
+      text: inputText,
+      done: false,
+    };
+    setTasks((prev) => [...prev, newTask]);
+    inputRef.current.value = "";
+  };
   return (
     <div className=" bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
-      
       {/* -----------title--------------  */}
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={todo_icon} alt="" />
@@ -14,18 +31,31 @@ export default function ToDo() {
       {/* -----------form --------------------- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none h-14 flex-1  pl-12 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           Add +
         </button>
       </div>
 
       {/* -----------todo list ------------------  */}
       <div>
-        <ToDoList />
+        {tasks.map((item, index) => {
+          return (
+            <ToDoList
+              key={index}
+              text={item.text}
+              id={item.id}
+              done={item.done}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,14 @@
 import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
-export default function ToDoList() {
+export default function ToDoList({text ,id ,done}) {
   return (
     <div className="flex items-center my-3 gap-2 ">
-      <div>
-        <img src={tick} alt="" />
-        <p>Learn coding ...</p>
+      <div className="flex flex-1 items-center cursor-pointer">
+        <img className="2-7" src={tick} alt="" />
+        <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
       </div>
+      <img className="w-4" src={delete_icon}  alt="" />
     </div>
   );
 }
